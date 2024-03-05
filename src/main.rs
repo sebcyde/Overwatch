f54126c0e482360853442038c0425e849b46b982
Sebastian Cyde <sebastian.cyde@red-engine.com>
2024-03-05 17:59:15 +0000

    Desktop notifications

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,55 @@
+extern crate notify_rust;
 extern crate sysinfo;
 
-use sysinfo::{DiskUsage, System};
+use notify_rust::Notification;
+use sysinfo::System;
+
+fn notify(title: &str, body: &str) {
+    _ = Notification::new().summary(title).body(body).show();
+}
 
 fn main() {
     let mut system = System::new_all();
 
-    loop {
-        system.refresh_all();
+    notify("Title", "Body copy");
 
-        // Get CPU usage
-        let cpu_usage = system.global_cpu_info();
-        println!("CPU Usage: {:?}%\n", cpu_usage);
+    // loop {
+    system.refresh_all();
 
-        let one: u64 = system.free_memory();
-        println!("Free Memory: {}n", one);
+    // Get CPU usage
+    let cpu_usage = system.global_cpu_info();
+    println!("CPU Usage: {:?}%\n", cpu_usage);
 
-        let cores: usize = system.physical_core_count().unwrap();
-        println!("CPU Usage: {:?}%\n", cores);
+    let one: u64 = system.free_memory();
+    println!("Free Memory: {}\n", one);
 
-        let processes = system.processes();
-        for (pid, process) in processes {
-            println!("PID: {}", pid);
-            println!("Name: {}", process.name());
+    let cores: usize = system.physical_core_count().unwrap();
+    println!("CPU Usage: {:?}%\n", cores);
 
-            // let disk_usage: DiskUsage = process.disk_usage();
-            // println!(
-            //     "read bytes   : new/total => {}/{}",
-            //     disk_usage.read_bytes, disk_usage.total_read_bytes,
-            // );
-            // println!(
-            //     "written bytes: new/total => {}/{}",
-            //     disk_usage.written_bytes, disk_usage.total_written_bytes,
-            // );
+    // let processes = system.processes();
 
-            println!("Process Memory Usage: {:?}\n", process.memory());
-        }
+    let available_memory: u64 = system.available_memory();
+    println!("Available Memory: {}\n", available_memory);
 
-        std::thread::sleep(std::time::Duration::from_secs(60));
-    }
+    // for (pid, process) in processes {
+    // println!("PID: {}", pid);
+    // println!("Name: {}", process.name());
+
+    // let disk_usage: DiskUsage = process.disk_usage();
+    // println!(
+    //     "read bytes   : new/total => {}/{}",
+    //     disk_usage.read_bytes, disk_usage.total_read_bytes,
+    // );
+    // println!(
+    //     "written bytes: new/total => {}/{}",
+    //     disk_usage.written_bytes, disk_usage.total_written_bytes,
+    // );
+    //
+    // println!("{} Memory Usage: {:?}\n", process.name(), process.memory());
+    // }
+
+    std::process::exit(0)
+
+    // std::thread::sleep(std::time::Duration::from_secs(60));
+    // }
 }
